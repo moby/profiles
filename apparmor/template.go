491da4e02c6b39a4f12d3e544630266dd3b950a4
Paweł Gronowski <pawel.gronowski@docker.com>
2026-09-21 12:30:51 +0200

    apparmor: Disable the ISDN socket family
    Signed-off-by: Paweł Gronowski <pawel.gronowski@docker.com>

--- apparmor/template.go
+++ apparmor/template.go
@@ -42,6 +42,7 @@ profile "{{.Name}}" flags=(attach_disconnected,mediate_deleted) {
   deny network atmsvc,
   deny network irda,
   deny network wanpipe,
+  deny network isdn,
   # Disallow AF_ALG (Linux kernel crypto API); see https://copy.fail/
   deny network alg,
   # Disallow AF_VSOCK to prevent host/guest communication.
